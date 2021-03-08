5628ee71c109821a51d5b2abb9a0a8f407321466
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 16:45:46 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let cfg   = rebuildConfig(bracketsConfig);
+    let stack = new Stack();
+
+    for (let x of str) {
+        if (stack.isEmpty() || cfg[x] != stack.head()) {
+            stack.push(x);
+        } else {
+            stack.pop();
+        }
+    }
+    return stack.isEmpty();
+}
+
+function rebuildConfig(bracketsConfig) {
+    let cfg = {};
+    for (let pair of bracketsConfig) {
+        cfg[pair[1]] = pair[0];
+    }
+    return cfg;
+}
+
+class Stack {
+    constructor() {
+        this._list = [];
+    }
+
+    push(x) {
+        this._list.push(x);
+    }
+    
+    pop() {
+        return this._list.pop();
+    }
+
+    head() {
+        return this._list[this._list.length - 1];
+    }
+
+    isEmpty() {
+        return this._list.length == 0;
+    }
 }
